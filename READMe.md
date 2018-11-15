609e81bbccad099761c1a0ac1a956e6b47939957
ACpython <10290348+Kagerjay@users.noreply.github.com>
2018-11-15 16:04:04 -0500

    First commit

---
+++ READMe.md
@@ -0,0 +1,13 @@
+GulpJS-Pug-Sass-Typescript Starter Kit is a simple, customizable frontend kit.
+
+# File structure
+
+# Installation
+
+```
+npm install gulp-cli -g
+```
+
+```
+npm install
+```
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var gulp = require("gulp");
+var pug = require("gulp-pug");
+var sass = require("gulp-sass");
+var cssnano = require("gulp-cssnano");
+var sourcemaps = require("gulp-sourcemaps");
+var autoprefixer = require("gulp-autoprefixer");
+
+gulp.task("workflow", function() {
+  gulp
+    .src("./src/scss/**/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: false
+      })
+    )
+    .pipe(cssnano())
+    .pipe(sourcemaps.write("./"))
+
+    .pipe(gulp.dest("./dist/css"));
+});
+
+//Watch task
+gulp.task("default", function() {
+  gulp.watch("./src/scss/**/*.scss", ["workflow"]);
+});
