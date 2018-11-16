7cda7874de8b658a96be99ff45d582ab230d996b
ACpython <10290348+Kagerjay@users.noreply.github.com>
2018-11-16 11:13:38 -0500

    Working browsersync gulp

--- gulpfile.js
+++ gulpfile.js
@@ -26,13 +26,13 @@ gulp.task("workflow", function() {
     .pipe(cssnano())
     .pipe(sourcemaps.write("./"))
 
-    .pipe(gulp.dest("./dist/css"));
-  // .pipe(browserSync.stream());
+    .pipe(gulp.dest("./dist/css"))
+    .pipe(browserSync.stream());
 });
 
-gulp.task("default", function() {
-  gulp.watch("./src/scss/**/*.scss", ["workflow"]);
-});
+// gulp.task("default", function() {
+//   gulp.watch("./src/scss/**/*.scss", ["workflow"]);
+// });
 
 /* PUG COMPILATION WATCH AND SAVE*/
 
@@ -49,23 +49,19 @@ gulp.task("views", function() {
   // .pipe(browserSync.stream());
 });
 
-gulp.task("default", function() {
-  gulp.watch("./src/pug/**/*.pug", ["views"]);
-});
-
 /* BROWSER SYNC */
 
-// gulp.task("serve", function() {
-//   browserSync.init({
-//     server: {
-//       baseDir: "./dist"
-//     }
-//   });
-//   gulp.watch("./src/pug/**/*.pug").on("change", browserSync.reload);
-//   gulp.watch("./src/scss/**/*.scss").on("change", browserSync.reload);
-//   gulp.watch("./src/ts/*.ts").on("change", browserSync.reload);
-// });
+gulp.task("serve", function() {
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    }
+  });
+  // gulp.watch("./src/ts/*.ts").on("change", browserSync.reload);
+  gulp.watch("./src/scss/**/*.scss", ["workflow"]);
+  gulp.watch("./src/pug/**/*.pug", ["views"]).on("change", browserSync.reload);
+});
 
 // /* SERVE */
 
-// gulp.task("default", ["serve"]);
+gulp.task("default", ["serve"]);
