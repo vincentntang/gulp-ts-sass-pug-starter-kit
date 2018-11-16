80a703d672eee65bff3c5f655e8151d8f05779e8
ACpython <10290348+Kagerjay@users.noreply.github.com>
2018-11-16 10:15:13 -0500

    Working Compilation PUG to HTML, but partials included in final

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var sass = require("gulp-sass");
 var cssnano = require("gulp-cssnano");
 var sourcemaps = require("gulp-sourcemaps");
 var autoprefixer = require("gulp-autoprefixer");
+var watch = require("gulp-watch");
 
 gulp.task("workflow", function() {
   gulp
@@ -24,20 +25,66 @@ gulp.task("workflow", function() {
     .pipe(gulp.dest("./dist/css"));
 });
 
-gulp.task("workflow", function buildHTML() {
-  gulp.src("./src/pug/**/*.pug").pipe(
-    pug({
-      doctype: "html",
-      pretty: false
-    }).pipe(gulp.dest("./dist/"))
-  );
-});
-
 //Watch task
 gulp.task("default", function() {
   gulp.watch("./src/scss/**/*.scss", ["workflow"]);
 });
 
-gulp.task("default", function() {
-  gulp.watch("./src/pug/**/*.pug", ["workflow"]);
+// gulp.task("pug", function buildHTML() {
+//   gulp
+//     .src(["src/pug/**/*.pug"], {
+//       base: "src/pug"
+//     })
+//     .pipe(pug())
+//     .pipe(gulp.dest("./dist/"));
+// });
+
+// gulp.task("templates", function() {
+//   gulp
+//     .src("./*.pug")
+//     .pipe(plumber({ errorHandler: onError }))
+//     .pipe(pug())
+//     .pipe(gulp.dest("build/"));
+// });
+
+gulp.task("views", function() {
+  return gulp
+    .src("./src/pug/**/*.pug")
+    .pipe(
+      pug({
+        doctype: "html",
+        pretty: true
+      })
+    )
+    .pipe(gulp.dest("./dist/"));
 });
+// gulp.task("workflow", function buildHTML() {
+//   return gulp.src("./src/pug/**/*.pug").pipe(
+//     pug({
+//       doctype: "html",
+//       pretty: false
+//     }).pipe(gulp.dest("./dist/"))
+//   );
+// });
+
+// gulp.task("pug", function() {
+//   return gulp
+//     .src("./src/pug/**/*.pug")
+//     .pipe(
+//       pug({
+//         doctype: "html",
+//         pretty: false
+//       })
+//     )
+//     .pipe(gulp.dest("./dist/"));
+// });
+
+gulp.task("default", function() {
+  gulp.watch("./src/pug/**/*.pug", ["views"]);
+});
+
+// gulp.task("watch", function() {
+//   return watch("./src/pug/**/*.pug", { ignoreInitial: false }).pipe(
+//     gulp.dest("pug")
+//   );
+// });
